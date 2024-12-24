0a0c967ac2354825f7a032d0a76ea7a9212959ab
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-12-24 11:33:53 -0300

    first commit

---
+++ src/main/java/com/larissa/rest_with_springBoot_and_java/RestWithSpringBootAndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.larissa.rest_with_springBoot_and_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larissa/rest_with_springBoot_and_java/RestWithSpringBootAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissa.rest_with_springBoot_and_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
